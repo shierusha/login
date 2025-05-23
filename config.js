bd79d64736ed602f4c548c730270e9d0580e7fa3
Roro <tinacat1518@gmail.com>
2025-05-23 22:52:43 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-
-    const SUPABASE_URL = 'https://jtijaauoeqpyyoicpcor.supabase.co';
-    const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0aWphYXVvZXFweXlvaWNwY29yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MzIwOTQsImV4cCI6MjA2MjIwODA5NH0.2wwDuo8wMtmNIPaidTsTOjlZeqngq7g3w32uTXn3VM0';
-    const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-   
+const SUPABASE_URL = 'https://jtijaauoeqpyyoicpcor.supabase.co';
+const SUPABASE_KEY = '你的 key';
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);

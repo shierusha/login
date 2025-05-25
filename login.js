bd59e93bf2054f0dfe69e9fdc667b53c9656ea8d
Roro <tinacat1518@gmail.com>
2025-05-26 02:04:08 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,6 @@
 // ====== Supabase 設定 ======
  const supabase = createClient('https://wfhwhvodgikpducrhgda.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmaHdodm9kZ2lrcGR1Y3JoZ2RhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMTAwNjEsImV4cCI6MjA2MzU4NjA2MX0.P6P-x4SxjiR4VdWH6VFgY_ktgMac_OzuI4Bl7HWskz8');
 
-
-
 // ===== 錯誤訊息轉換 =====
 function transErrorMsg(msg) {
   if (!msg) return '';

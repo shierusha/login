2a37f4cc4f3697d1e0678e12328812884c51375c
Roro <tinacat1518@gmail.com>
2025-05-26 01:59:52 +0900

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,6 @@
 // ====== Supabase 設定 ======
-const SUPABASE_URL = 'https://jtijaauoeqpyyoicpcor.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0aWphYXVvZXFweXlvaWNwY29yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MzIwOTQsImV4cCI6MjA2MjIwODA5NH0.2wwDuo8wMtmNIPaidTsTOjlZeqngq7g3w32uTXn3VM0';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+ const supabase = createClient('https://wfhwhvodgikpducrhgda.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmaHdodm9kZ2lrcGR1Y3JoZ2RhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMTAwNjEsImV4cCI6MjA2MzU4NjA2MX0.P6P-x4SxjiR4VdWH6VFgY_ktgMac_OzuI4Bl7HWskz8');
+
 
 
 // ===== 錯誤訊息轉換 =====
